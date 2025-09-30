74c66174b60694187eb79457c259611b658980ee
maxiecuti <juhjuh.karla@gmail.com>
2025-09-30 08:55:38 -0300

    adicionei o rest controller no administrador

--- src/main/java/controllers/AdministradorController.java
+++ src/main/java/controllers/AdministradorController.java
@@ -1,5 +1,10 @@
 package controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cadastro_administrador")
 public class AdministradorController {
 
 }
