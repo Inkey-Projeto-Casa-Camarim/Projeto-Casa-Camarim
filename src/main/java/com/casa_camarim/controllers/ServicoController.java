b668295502d82fb721e570d819d0de2312a6938b
maxiecuti <juhjuh.karla@gmail.com>
2025-10-02 15:21:40 -0300

    Adicionei o PutMapping no Agenda e no Serviço

--- src/main/java/com/casa_camarim/controllers/ServicoController.java
+++ src/main/java/com/casa_camarim/controllers/ServicoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,12 @@ public class ServicoController {
 	@GetMapping("/{id}")
 	public Servico getServicoById(@PathVariable Long id) {
 		return servicoService.getServicoById(id);
+	}
+	
+	// Endpoint para editar os serviços
+	@PutMapping
+	public Servico editServico(@RequestBody Servico servico) {
+		return servicoService.saveServico(servico);
 	}
 
     //Endpoint para deletar um serviço
