1de0ccd2891920ac09c124d146571ecf490ac7b0
maxiecuti <juhjuh.karla@gmail.com>
2025-10-02 14:15:00 -0300

    Adaptei o data.sql para os atributos do projeto

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
-insert ignore into TipoUsuario(id_tipo, nome_tipo, cpf)
-values (1, 'Cliente'), (2, 'Admin');
+insert ignore into tipo_usuario (id_tipo, nome_tipo, cpf) 
+values (1, 'Cliente', '00000000000'), (2, 'Admin',   '11111111111');
 
-insert ignore into tb_usuario(id_usuario, nome, telefone, tipo_cliente_id)
-values (1, 'admin@gmail.com', 'AdminQu8', 'admin32u', 2)
+
+insert ignore into tb_usuario(id_usuario, nome, telefone)
+values (1, 'Administrador', '15999999999');
