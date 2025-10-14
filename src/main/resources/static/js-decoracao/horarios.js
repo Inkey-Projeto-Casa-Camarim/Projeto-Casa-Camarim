6b8eed3118cc14f35c6a6a7a5179867d844143c1
Blightpb <rafaelapaz2008@gmail.com>
2025-10-14 15:56:05 -0300

    continuidade na página de adm

--- src/main/resources/static/js-decoracao/horarios.js
+++
@@ -1,7 +0,0 @@
- // Seleciona todos os serviços e adiciona evento em cada botão
-        document.querySelectorAll(".servicos").forEach(servico => {
-            const botao = servico.querySelector(".agendar");
-            botao.addEventListener("click", () => {
-                servico.classList.toggle("active");
-            });
-        });
