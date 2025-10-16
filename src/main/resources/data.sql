81c520d223f228ffdf229f7631fcff6bc72b66a2
maxiecuti <juhjuh.karla@gmail.com>
2025-10-16 07:20:37 -0300

    Alteração na tabela tipo_usuario, cpf para senha

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,11 @@
 -- Insere registros na tabela tipo_usuario, ignorando conflitos de chave primária ou únicos
-insert ignore into tipo_usuario (id_tipo, nome_tipo, cpf) 
-values (1, 'Cliente', '00000000000'), -- Insere o tipo de usuário "Cliente" com CPF fictício
-(2, 'Admin',   '11111111111'); -- Insere o tipo de usuário "Admin" com CPF fictício
+insert ignore into tipo_usuario (id_tipo, nome_tipo, senha) 
+values (1, 'CasaCamarim', '12345'); -- Insere o tipo de usuário "Admin" com CPF fictício
 
 -- Insere um registro na tabela "tb_usuario" com os valores especificados.
 -- "insert ignore" evita erro caso já exista um registro com o mesmo "id_usuario" (não insere duplicatas).
 insert ignore into tb_usuario(id_usuario, nome, telefone)
-values (1, 'Administrador', '15999999999'); -- Valores dos campos inseridos
+values (1, 'Cliente', '15999999999'); -- Valores dos campos inseridos
 
 -- Tabela de serviço com os serviços oferecidos pela casa camarim
 insert ignore into tb_servico(id_servico, preco, nome_servico, descricao_servico, tempo_servico)
