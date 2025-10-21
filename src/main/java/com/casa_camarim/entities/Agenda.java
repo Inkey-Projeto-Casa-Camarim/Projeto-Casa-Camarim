aabc539e323563dbdc05fc0589c6b49bf91e1973
GabriellaMoura7 <159180575+GabriellaMoura7@users.noreply.github.com>
2025-10-21 10:11:02 -0300

    Agenda entidade
    Foi adicionado o @ManytoOne, @JoinColumn e @JsonIgnoreProerties.

--- src/main/java/com/casa_camarim/entities/Agenda.java
+++ src/main/java/com/casa_camarim/entities/Agenda.java
@@ -1,10 +1,14 @@
 package com.casa_camarim.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,33 +25,52 @@ public class Agenda {
 	@Column(name = "data", nullable = false)
 	private String data;
 	
+	@ManyToOne
+	@JoinColumn(name = "id_usuario", nullable = false)
+	@JsonIgnoreProperties("usuarios")
+	private Usuario usuario;
+	
 	// Construtores(método usado para criar objetos)
 	
 	public Agenda() {
-		
 	}
-	
-	public Agenda(long id_agenda, String data) {
+
+		
+	public Agenda(Long id_agenda, String data, Usuario usuario) {
+		super();
 		this.id_agenda = id_agenda;
 		this.data = data;
+		this.usuario = usuario;
 	}
 
 	// Getters e Setters(getters -> obtém o valor de um atributo, setters -> altera o valor de um atributo)
-	
+
 	public Long getId_agenda() {
 		return id_agenda;
 	}
+
 
 	public void setId_agenda(Long id_agenda) {
 		this.id_agenda = id_agenda;
 	}
 
+
 	public String getData() {
 		return data;
 	}
 
+
 	public void setData(String data) {
 		this.data = data;
 	}
-	
+
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
