b1a97dd10092674f729e559edbfa16839d70af3d
GabriellaMoura7 <159180575+GabriellaMoura7@users.noreply.github.com>
2025-10-28 09:01:53 -0300

    Correção dos comentários e as importações nos códigos.

--- src/main/java/com/casa_camarim/repositories/ServicoRepository.java
+++ src/main/java/com/casa_camarim/repositories/ServicoRepository.java
@@ -2,7 +2,9 @@ package com.casa_camarim.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.casa_camarim.entities.Servico;
+
 //Interface responsável por acessar o banco de dados da entidade Servico
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
- // Métodos CRUD já disponíveis pelo JpaRepository
+ // Métodos básicos de acesso ao banco já fornecidos pelo JpaRepository
 }
