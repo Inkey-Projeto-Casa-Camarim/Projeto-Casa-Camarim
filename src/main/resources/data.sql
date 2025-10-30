94f80bf92ea44ba557aae4a7550f9b75da25ec94
Blightpb <rafaelapaz2008@gmail.com>
2025-10-30 08:10:29 -0300

    Projeto inicializando corretamente

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 -- Insere registros na tabela tipo_usuario, ignorando conflitos de chave primária ou únicos
-insert ignore into tb_tipoUsuario (id_tipoUsuario, nome_tipo, senha, cpf) 
-values (1, 'Cliente', '12345', '00000000000'), -- Insere o tipo de usuário "Cliente" com CPF fictício
-(2, 'Admin', '12334', '11111111111'); -- Insere o tipo de usuário "Admin" com CPF fictício
+insert ignore into tipo_usuario (id_tipo, nome_tipo, cpf) 
+values (1, 'Cliente', '00000000000'), -- Insere o tipo de usuário "Cliente" com CPF fictício
+(2, 'Admin',   '11111111111'); -- Insere o tipo de usuário "Admin" com CPF fictício
 
 -- Insere um registro na tabela "tb_usuario" com os valores especificados.
 -- "insert ignore" evita erro caso já exista um registro com o mesmo "id_usuario" (não insere duplicatas).
